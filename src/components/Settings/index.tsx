fed190d59effd0f908973e33be76881a73e746af
Marcos Duarte <marcosduarte1994@gmail.com>
2023-11-15 11:56:20 -0300

    feat: add features framer motion

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -42,7 +42,16 @@ export function SettingsTabs() {
           title="Notifications"
           isSelected={currentTab === 'tab8'}
         />
-        <TabItem value="tab9" title="API" isSelected={currentTab === 'tab9'} />
+        <TabItem
+          value="tab9"
+          title="Intergrations"
+          isSelected={currentTab === 'tab9'}
+        />
+        <TabItem
+          value="tab10"
+          title="API"
+          isSelected={currentTab === 'tab10'}
+        />
       </Tabs.List>
     </Tabs.Root>
   )
